700c13161fc9c95d76b099e71c13b862c640ebb8
jmoypace <jmoypace@gmail.com>
2018-06-23 15:29:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma method
-puts "I LOVE YOU GRANDMA!"
+puts "I LOVE YOU TOO PUMPKIN!"
 
 end
 # Whatever you say to grandma, she should respond with
